7922c72fccb23202eda1e3c87462eb3775b31471
JonathanComarella <jonathanwcomarella@gmail.com>
2021-12-08 23:58:53 -0300

    Created Books and BooksResource

---
+++ backend/src/main/java/com/jc/livraria/entities/Books.java
@@ -0,0 +1,73 @@
+package com.jc.livraria.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Books implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String author;
+	private Integer anoPublicacao;
+
+	public Books() {
+	}
+
+	public Books(Integer id, String name, String author, Integer anoPublicacao) {
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getAnoPublicacao() {
+		return anoPublicacao;
+	}
+
+	public void setAnoPublicacao(Integer anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Books other = (Books) obj;
+		return Objects.equals(id, other.id);
+	}
+}
